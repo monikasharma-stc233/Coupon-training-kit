417b533d5db7933a01011ab360d6b8ea66848674
Monika Sharma <monika.ii@seventhtriangle.com>
2026-06-03 17:23:29 +0530

    catchAsync_Used

--- skeleton/src/controllers/couponController.js
+++ skeleton/src/controllers/couponController.js
@@ -1,28 +1,17 @@
 import * as couponService from '../services/couponService.js';
+import { catchAsync } from '../utils/helpers.js';
 
-export async function createCoupon(req, res, next) {
-  try {
-    const coupon = await couponService.createCoupon(req.body);
-    return res.status(201).json(coupon);
-  } catch (err) {
-    next(err);
-  }
-}
+export const createCoupon = catchAsync(async (req, res) => {
+  const coupon = await couponService.createCoupon(req.body);
+  return res.status(201).json(coupon);
+});
 
-export async function getCoupon(req, res, next) {
-  try {
-    const coupon = await couponService.getCouponByCode(req.params.code);
-    return res.status(200).json(coupon);
-  } catch (err) {
-    next(err);
-  }
-}
+export const getCoupon = catchAsync(async (req, res) => {
+  const coupon = await couponService.getCouponByCode(req.params.code);
+  return res.status(200).json(coupon);
+});
 
-export async function updateCoupon(req, res, next) {
-  try {
-    const coupon = await couponService.updateCoupon(req.params.code, req.body);
-    return res.status(200).json(coupon);
-  } catch (err) {
-    next(err);
-  }
-}
+export const updateCoupon = catchAsync(async (req, res) => {
+  const coupon = await couponService.updateCoupon(req.params.code, req.body);
+  return res.status(200).json(coupon);
+});
--- skeleton/src/controllers/redemptionController.js
+++ skeleton/src/controllers/redemptionController.js
@@ -1,21 +1,14 @@
 import * as redemptionService from '../services/redemptionService.js';
+import { catchAsync } from '../utils/helpers.js';
 import { parseUserId } from '../utils/redemptionHelpers.js';
 
-export async function redeemCoupon(req, res, next) {
-  try {
-    const userId = parseUserId(req.headers['x-user-id']);
-    const result = await redemptionService.redeemCoupon(userId, req.body);
-    return res.status(200).json(result);
-  } catch (err) {
-    next(err);
-  }
-}
+export const redeemCoupon = catchAsync(async (req, res) => {
+  const userId = parseUserId(req.headers['x-user-id']);
+  const result = await redemptionService.redeemCoupon(userId, req.body);
+  return res.status(200).json(result);
+});
 
-export async function revertRedemption(req, res, next) {
-  try {
-    const result = await redemptionService.revertRedemption(req.params.id);
-    return res.status(200).json(result);
-  } catch (err) {
-    next(err);
-  }
-}
+export const revertRedemption = catchAsync(async (req, res) => {
+  const result = await redemptionService.revertRedemption(req.params.id);
+  return res.status(200).json(result);
+});
